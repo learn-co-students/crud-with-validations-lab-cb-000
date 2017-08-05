d9a970154a2249cd3fba7833261991c8908e8271
john vascsinec <pensteeler@gmail.com>
2017-08-05 03:14:29 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/delete'
+
+  resources :songs, only: [:index, :show, :new, :create, :edit, :delete, :update, :destroy]
 end
