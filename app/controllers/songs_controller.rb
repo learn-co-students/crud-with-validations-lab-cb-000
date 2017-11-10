8b68873459cbf2d3acf9e10deb13fdecd3990d45
sjmoody <steven@beachheadmarketing.com>
2017-11-10 17:35:14 +0100

    bug: creation of new song instance doesn't capture title value from form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,39 @@
 class SongsController < ApplicationController
+require 'pry'
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find_by(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+
+  def create
+
+    @song = Song.new(song_params)
+    binding.pry
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find_by(params[:id])
+
+  end
+
+  def update
+  end
+
+  def song_params
+    params.permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 end
