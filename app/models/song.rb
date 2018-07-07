bb9ee289d7f8fbf943c9aec1b6eecdd1b617198c
Alex Segers <a.segers.dev@gmail.com>
2018-07-07 21:08:24 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,24 @@
+# rails g resource Song title:string released:boolean release_year:integer  artist_name:string genre:string --no-test-framework --no-assets --no-helper
 class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validate :title_is_unique_to_release_year
+  validate :release_year_if_released
+  validates :artist_name, presence: true
+
+  def title_is_unique_to_release_year
+    if released && Song.find_by_release_year(release_year)
+      errors.add(:title, "must be unique to the year released")
+    end
+  end
+
+  def release_year_if_released
+    if released
+      if !release_year || release_year == 0
+        errors.add(:release_year, "can't be blank if the song is released")
+      elsif release_year > Time.now.year
+        errors.add(:release_year, "can't be greater than the current year")
+      end
+    end
+  end
+
 end
