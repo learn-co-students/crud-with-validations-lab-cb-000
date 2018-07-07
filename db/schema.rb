52e4e29f5f247c69c2c28e8d0fcda868d670a5bc
Alex Segers <a.segers.dev@gmail.com>
2018-07-07 22:42:30 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20180707203755) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.boolean  "released"
+    t.boolean  "released",     default: false
     t.integer  "release_year"
     t.string   "artist_name"
     t.string   "genre"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
 end
