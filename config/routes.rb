d51e9e99a87ad7e87af545d40707daf114fcf55a
nichia <nichia@gmail.com>
2018-07-29 16:00:33 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Rails.application.routes.draw do
+  resources :songs
+
+#  Helper  HTTP   Path
+#  Prefix  Verb   URI Pattern               Controller#Action
+# Path/Url
+# songs    GET    /songs(.:format)          songs#index
+#          POST   /songs(.:format)          songs#create
+#new_song  GET    /songs/new(.:format)      songs#new
+#edit_song GET    /songs/:id/edit(.:format) songs#edit
+#  song    GET    /songs/:id(.:format)      songs#show
+#          PATCH  /songs/:id(.:format)      songs#update
+#          PUT    /songs/:id(.:format)      songs#update
+#          DELETE /songs/:id(.:format)      songs#destroy
 end
