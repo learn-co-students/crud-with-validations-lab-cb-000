f6d6e77bfc785a55a82c3a406b362fc80ba0adb5
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-02-28 12:30:00 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,21 +3,22 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :artist_name, presence: true
-  # validates :release_year, inclusion: { in: 0..Date.today.year }
   validates :title, uniqueness: { scope: [:release_year, :artist_name] }
   validate :released_song_has_year?
   validate :song_year_correct?
 
   def released_song_has_year?
-    if (released == true && !release_year.present?) || released == false
-      errors.add(:release_year, "No release year")
+    if released == true
+      if !release_year.present?
+        errors.add(:release_year, "No release year")
+      end
     end
   end
 
   def song_year_correct?
-    binding.pry
+    # binding.pry
     if released == true
-      if !0..Date.today.year.include?(release_year)
+      if !(0..Date.today.year).include?(release_year)
         errors.add(:release_year, "No release year")
       end
     end
