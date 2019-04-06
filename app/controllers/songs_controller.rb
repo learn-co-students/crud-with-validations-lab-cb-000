946cfc9636225400fc92c5fc27ea5266b649e5af
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-06 09:36:44 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,7 +37,7 @@ class SongsController < ApplicationController
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    redirect_to songs_url
+    redirect_to songs_path
   end
 
   private
