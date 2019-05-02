3cfd0545e75a3816e1fd646e05c152c4b8ea0f6c
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-02 17:38:56 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: { scope: :release_year, :artist_name,
-    message: "Same title cannot occur more than once in the same year by the same artist" }
+  validates :title, uniqueness: { scope: [:release_year, :artist_name] }
   validates :released, inclusion: { in: [true, false] }
   validates :release_year, presence: true, if: :released?
   validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.new.year }, if: :released?
