fa083f1cfe779ef3a36ee7e5d9298db0117c20e0
kenkuts <kennethchoi19@gmail.com>
2019-06-20 18:29:04 -0400

    completed validations from models

---
+++ app/models/song.rb
@@ -0,0 +1,15 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: :release_year, message: "Song with the same title should be released in a different year."}
+  validates :artist_name, presence: true
+  validates :release_year, presence: true, if: :released
+  validates :released, inclusion: { in: [true, false] }
+  validate :year_restriction
+  validates :genre, presence: true
+
+  def year_restriction
+    if (release_year != nil) && (release_year > Date.today.year)
+      errors.add(:date, "Date can't be in the future!")
+    end
+  end
+
+end
